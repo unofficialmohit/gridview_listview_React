7c9bd98d2a628beace0f79bf60e8de54197a5c14
mohit <mohitgujjar2121@gmail.com>
2024-03-22 17:26:38 +0530

    main

--- src/DisplayData.jsx
+++ src/DisplayData.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Data from "./Data";
-async function  dataFetch()
-{
- const response= await fetch("https://jsonplaceholder.typicode.com/todos");
-  const data=await response.json();
-  return data;
-}
-const data=await dataFetch();
+let data;
+
+console.log(data);
+
 function DisplayData(){
-const[dataToDisplay,setDataToDisplay]=React.useState(data);
+const[dataToDisplay,setDataToDisplay]=React.useState([]);
+React.useEffect(()=>{
+  const fetchData= async ()=>
+  {
+   const response= await fetch("https://jsonplaceholder.typicode.com/todos");
+    const result=await response.json();
+    setDataToDisplay(result);
+  }
+  fetchData();
+},[]);
 const[gridView,setGridView]=React.useState(false);
 console.log(dataToDisplay);
 function handleClick(){
@@ -18,8 +24,8 @@ return(
     <div>
     <button onClick={handleClick} className="btn">CLICK HERE TO CHANGE UI</button>
     <div className={gridView?"gridcontainer":"listcontainer"}>
-     {dataToDisplay.map((item)=>{
-         return <Data detail={item} grid={gridView}/>
+     {dataToDisplay.map((item,index)=>{
+         return <Data key={index} detail={item} grid={gridView} />
         })}
     </div>
         </div>
